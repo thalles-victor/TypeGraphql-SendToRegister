6ea87ca595fcc8de541cc8c59c3b693991c8cadc
thalles-victor <thalles.victor2012@gmail.com>
2022-02-08 21:23:07 -0300

    Adicionado o banco de dados remoto

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,10 @@
 import 'reflect-metadata';
-import { ID, Field, ObjectType, Resolver, Query, buildSchema} from 'type-graphql';
+import { ID, Field, ObjectType, Resolver, Query, buildSchema, InputType, Mutation, Arg} from 'type-graphql';
 import { ApolloServer } from 'apollo-server';
 
+import { PrismaClient } from '@prisma/client';
 
-
-@ObjectType()
-class Recipe {
-  @Field( type => ID )
-  id: string;
-
-  @Field()
-  title: string;
-
-  @Field()
-  description?: string
-
-  @Field()
-  creationDate: Date;
-
-  @Field( type => [String] )
-  ingredients: string[];
-}
+const prisma = new PrismaClient();
 
 @ObjectType()
 class TypeMessage {
@@ -29,17 +13,63 @@ class TypeMessage {
 
 }
 
+@InputType()
+class UserInput {
+  @Field()
+  email: string;
+
+  @Field()
+  name: string;
+}
+
+@ObjectType()
+class UserObject {
+  @Field(type=> ID)
+  id: number;
+
+  @Field()
+  email: string;
+
+  @Field({nullable: true})
+  name?: string;
+}
+
 @Resolver()
 class RecipeResolver {
   @Query( returns => TypeMessage )
   async getMessage(){
     return { text: "Hello, TypeGraphQL" }
   }
+
+  @Mutation( returns => [UserObject] )
+  async resgister(
+    @Arg("data") data: UserInput
+  ): Promise<UserObject[] | undefined>{
+    await prisma.user.create({
+      data: {
+        email: data.email,
+        name: data.name
+      }
+    })
+
+    //
+    const user = await prisma.user.findMany()
+    console.log(
+      user
+    )
+    if(!user) return undefined
+
+    //
+
+    return user;
+
+  }
 }
 
 const PORT = process.env.PORT || 4000;
 
 const bootstrap = async () => {
+
   const schema = await buildSchema({
     resolvers: [RecipeResolver]
   })
